6eee6c3b6406ee6a35674a6b4cf4bea138059b74
cr0wnxd <48798731+cr0wnxd@users.noreply.github.com>
2020-11-07 05:45:59 +0400

    remade intro
    intro + main.js changed to <pre> onclick

--- js/audio.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("myVideo").volume = 0.1;
--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 document.getElementById("myVideo").volume = 0.1;
 var vid = document.getElementById("myVideo");
-$("#continueButton").click(function () {
+$("pre").click(function () {
 $("#intro").fadeOut(2500);
 setTimeout(function(){
-$("#main").fadeIn(2000);
+$("#main").fadeIn(3000);
 vid.play();vid
         }, 500)
 });
